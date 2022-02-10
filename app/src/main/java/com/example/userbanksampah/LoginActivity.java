65f74ecadb9b3feff7cca467c0268252d02d9724
Phaksi Bangun Asmoro <76397234+shibasmoro@users.noreply.github.com>
2022-02-10 14:18:48 +0700

    Revert "Development"

--- app/src/main/java/com/example/userbanksampah/LoginActivity.java
+++ app/src/main/java/com/example/userbanksampah/LoginActivity.java
@@ -1,55 +1,28 @@
 package com.example.userbanksampah;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.textfield.TextInputEditText;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
    TextInputEditText username;
-   TextView textView , Id,password;
-
-   Button login;
+   TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_login);
-        init();
         textView = findViewById(R.id.register);
-        login = findViewById(R.id.btnlogin);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NasabahImpl.login().validate_login(Id.getText().toString(),password.getText().toString()).enqueue(new Callback<String>() {
-                    @Override
-                    public void onResponse(Call<String> call, Response<String> response) {
-                        if (response.isSuccessful()){
-                            Toast.makeText(getApplicationContext(), response.body(), Toast.LENGTH_LONG).show();
-                        }else{
-                            Toast.makeText(getApplicationContext(), response.body(), Toast.LENGTH_LONG).show();
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<String> call, Throwable t) {
-
-                    }
-                });
-            }
-        });
-        textView.setOnClickListener(new View.OnClickListener() {
+    textView.setOnClickListener(new View.OnClickListener() {
         @Override
         public void onClick(View view) {
             startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
@@ -57,17 +30,6 @@ public class LoginActivity extends AppCompatActivity {
     });
     }
 
-        // saat tombol login ditekan
-
-
     public void login(View view) {
-
-    }
-
-    // initialitation
-    public void init (){
-        Id =findViewById(R.id.edittext);
-        password =findViewById(R.id.edittextpassword);
-
     }
 }
